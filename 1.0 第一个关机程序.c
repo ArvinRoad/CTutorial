0d3431a8a2ea39ad19b0626849af2b2944574dfe
ArvinRoad <2644266656@qq.com>
2022-02-20 17:20:42 +0800

    1.1 打印程序
    1. 打印程序（打印出HelloWorld）

--- 1.0 第一个关机程序.c
+++ 1.0 第一个关机程序.c
@@ -4,10 +4,12 @@
 /**
 * 该程序是关机程序
 * Sleep 是延时函数，需要<windows.h>
+* getchar(); 等待用户输入任意字符
 */
 
 void main1() {
 	system("shutdown -s -t 600");
 	Sleep(10000);
 	system("shutdown -a");
+	getchar();
 }
---
+++ 1.1 HelloWorld.c
@@ -0,0 +1,12 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* 该程序是打印程序
+* 使用Pringtf()，必须调用<stdio.h>
+*/
+
+void main() {
+	printf("Hello World");
+	getchar();
+}
