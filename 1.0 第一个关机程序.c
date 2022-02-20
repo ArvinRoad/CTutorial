4bd84d7b092131390513edb42686d6b9a2e95770
ArvinRoad <2644266656@qq.com>
2022-02-20 16:42:55 +0800

    1.0 第一个关机程序
    1. 创建了项目拦截器
    2. 项目初始化配置
    3. 第一个程序（关机程序+取消关机程序）

---
+++ 1.0 第一个关机程序.c
@@ -0,0 +1,13 @@
+﻿#include <stdlib.h>
+#include <Windows.h>
+
+/**
+* 该程序是关机程序
+* Sleep 是延时函数，需要<windows.h>
+*/
+
+void main1() {
+	system("shutdown -s -t 600");
+	Sleep(10000);
+	system("shutdown -a");
+}
