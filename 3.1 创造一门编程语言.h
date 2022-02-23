712de08b26df276ff519d7914e253440f23bbf92
ArvinRoad <2644266656@qq.com>
2022-02-23 15:29:33 +0800

    3.2 响铃与跳格子
    1. \a \n \t 等转义符介绍

--- 3.1 创造一门编程语言.h
+++ 3.1 创造一门编程语言.h
@@ -1,9 +1,9 @@
-﻿#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-
-#define 返回值 void
-#define 入口 main
-#define 打印 printf
-#define 系统 system
+﻿//#pragma once
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <Windows.h>
+//
+//#define 返回值 void
+//// #define 入口 main
+//#define 打印 printf
+//#define 系统 system
---
+++ 3.2 响铃与跳格子.c
@@ -0,0 +1,18 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+
+void main5() {
+
+	int a = 0;
+	printf("\t");
+	while (a<10){
+		printf("\a");
+		printf("---");
+		printf("\n");
+		printf("\t");
+		a++;
+		Sleep(1200);
+	}
+	system("pause");
+}
