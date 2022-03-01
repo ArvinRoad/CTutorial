691f554644df13897e9711031a7751d5223d8d54
ArvinRoad <2644266656@qq.com>
2022-03-01 11:21:13 +0800

    数据类型与进制转换
    1. 数据类型
    2. 进制转换

---
+++ 3.3 进制转换.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main6() {
+
+	printf("%d", 0x12,"\n"); // 十六进制(0X)
+	printf("%d", 012, "\n"); // 八进制(0)
+	printf("%d", 12, "\n");  // 十进制
+
+	system("pause");
+}
---
+++ 3.4 数据类型.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main7() {
+
+	int a = 10;
+	printf("%d\n",a);
+	printf("%lld\n", sizeof(a));	// 按照整数方式解析
+
+	system("pause");
+}
