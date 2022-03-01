773da6c80f2b6cbef4b81f82f776c1b30af87393
ArvinRoad <2644266656@qq.com>
2022-03-01 21:00:31 +0800

    数据极限与正负
    1. 数据极限与正负

---
+++ 3.5 数据极限与正负.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+*	INT_MAX | INT_MIN 包含在头文件<limits.h>(极限)中
+*/
+
+void main8() {
+	int num_max = INT_MAX;
+	int num_min = INT_MIN;
+
+	unsigned char ch = 255;
+
+	printf("ch的数值是：%d\n", ch);
+	printf("%d,%d\n", num_max, num_min);
+
+	system("pause");
+}
