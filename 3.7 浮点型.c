ea2837e008dda90b9d70c07fbf87b1f3e2470f43
ArvinRoad <2644266656@qq.com>
2022-03-02 14:01:13 +0800

    浮点型
    1. 浮点型

---
+++ 3.7 浮点型.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void main10() {
+
+	float a = 10.1f;		// 有效位数是6-7位
+	double b = 10.0;		// 默认是15-16位
+	long double c = 3.1415;	//
+
+	printf("%d,%d,%d\n", sizeof(float), sizeof(double), sizeof(long double));
+
+	// 12.3e3 == 12.3*10^3
+	// 1.2345e+002 == 1.2345*10正2次方
+	// 1.2345e-002 == 1.2345*10负2次方
+	system("pause");
+}
