cf16bec511d490b1caa6eb357a091902de7ff483
ArvinRoad <2644266656@qq.com>
2022-03-02 20:02:40 +0800

    加减乘除
    1. 加减乘除

---
+++ 3.13 加减乘除.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main16() {
+
+    int a = 10;
+    int b = 5;
+    int c = a+b;
+    printf("%d\n",c);
+
+    double d = a+b;
+    printf("%f\n",d);
+
+    float e = 10 / 3;
+    int f = e;
+    printf("%d\n%f\n",f,e);
+
+    int h = 10 % 2;
+    printf("%d\n",h);
+    if (h == 0) {
+        h++;
+        printf("%d\n",h);
+        if (h<2) {
+            ++h;
+            printf("%d\n",h);
+        }
+    }
+    
+    system("pause");
+}
