9515ed28a7c35854d33e5616b2e7fb29edd66fff
ArvinRoad <2644266656@qq.com>
2022-03-02 17:18:46 +0800

    隐式类型转换
    1. 隐式类型转换

---
+++ 3.11隐式类型转换.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main14() {
+
+    int a = 1;  // 4字节
+    double b = 0.5; // 8字节
+
+    printf("%f\n",a + b); // 输出为：1.500000，他们在8个字节环境下相加所有可以将int隐形转换为double。
+
+    system("pause");
+}
