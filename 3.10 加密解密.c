13eaca761f7b7f62a3bcd0565918caeb25d6e781
ArvinRoad <2644266656@qq.com>
2022-03-02 16:50:14 +0800

    加密与解密
    1. 加密与解密（用到了数组）

---
+++ 3.10 加密解密.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main13() {
+
+    char ch = getchar();    // 存储一个用户输入的单个字符,输入A获取0
+    if(ch<='F') { // 区间
+        ch = ch - 17;   //转换
+    }
+
+    putchar(ch);    //输出
+
+    char Ch[10] = {'c','a','l','c'};
+    system(Ch);
+
+    for (int i = 0; i < 4; i++){
+        Ch[i] = Ch[i] - 30;
+    }
+
+    printf("%s\n",Ch);
+    
+    system("pause");
+}
