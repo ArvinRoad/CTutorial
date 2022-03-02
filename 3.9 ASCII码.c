268a116f9cab562f564c35846339e8a9aac24a32
ArvinRoad <2644266656@qq.com>
2022-03-02 16:11:46 +0800

    ASCII码
    1. ASCII 码

---
+++ 3.9 ASCII码.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main12() {
+
+	int a = 1;
+	char b = '1';
+
+	printf("%d,%d\n", sizeof(a), sizeof(b));
+
+	putchar(73);		// 输出单个字符
+	putchar(76);
+	putchar(111);
+	putchar(118);
+	putchar(101);
+	putchar(89);
+	putchar(117);
+	putchar(111);
+
+	system("pause");
+}
