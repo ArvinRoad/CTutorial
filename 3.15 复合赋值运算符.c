f2bf4f568a500de25515c41d2212750338529008
ArvinRoad <2644266656@qq.com>
2022-03-03 03:43:54 +0800

    复合赋值运算符
    1. 复合赋值运算符

---
+++ 3.15 复合赋值运算符.c
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+void main18(){
+
+    int a = 5;
+    a+=10;
+    printf("%d\n",a); // 结果a = 15 | a = a +10
+
+    int A = 3;
+    int B = A += A -= A * A;    // 首先A*A = 9 | 然后是 A = -6 = 3 -9 | 最后 A = A + (-6) | 所以得出为 -12 
+    printf("%d\n",B);
+
+    system("pause");
+}
