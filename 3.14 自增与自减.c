9f141f3b7cc038c015ef83035fdbb52a467dc760
ArvinRoad <2644266656@qq.com>
2022-03-03 03:20:51 +0800

    自增与自减
    1. 自增与自减

---
+++ 3.14 自增与自减.c
@@ -0,0 +1,27 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+void main17() {
+
+    int a = 5;
+    a++;    // 先运算A，然后再进行加1
+    printf("%d\n",a++);
+
+    printf("%d\n",++a);
+    a--;
+    printf("%d\n",a);
+    printf("%d\n",--a);
+    
+    int A = 10;
+    int b = A++;
+    int c = ++b;
+    b = c*3;
+    c = --b;
+    b = ++c;
+    printf("%d\n",b);
+    
+    int C = (A++) + (++A) * 3 +(--A) + (A++); // 结合方向：从右到左 巨恶心的问题
+    printf("%d\n",C);
+    
+    system("pause");
+}
