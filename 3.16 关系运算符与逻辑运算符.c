ffd426eb84110f9b811fec1a07137b745a92b6f9
ArvinRoad <2644266656@qq.com>
2022-03-03 15:12:26 +0800

    关系运算与逻辑运算：Scanf语句
    1. 关系运算符与逻辑运算符
    2. Scanf语句

---
+++ 3.16 关系运算符与逻辑运算符.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * #define _CRT_SECURE_NO_WARNINGS 是解决scanf报错问题，因为它是之间对地址值的修改，为了安全起见编译器会进行审查
+ */
+
+int main19() {
+
+    int I = 520;
+    int Y = 1314;
+    while (I < Y) {
+        printf("I Love You\n");
+        Y-=1000;
+    }
+    printf("%d,%d\n",I,Y);
+
+    printf("请输入你心里的数值：");
+    scanf("%d", &I);    // 或采用 scanf_s("%d",I);
+    printf("%d\n",I);
+    if (I == 1314 && I > Y) {
+     printf("祝有情人白头到老\n");   
+    }
+    else if (I == 520 || I > Y) {
+        printf("爱情是两个人努力的结果\n");
+    }else {
+        printf("缘分还是不够呀~\n");
+    }
+    
+    system("pause");
+}
