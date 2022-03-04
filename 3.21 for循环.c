8cc803a05f9e6da31d330b09f9fc3c05a1bd6a13
ArvinRoad <2644266656@qq.com>
2022-03-04 20:05:16 +0800

    for 循环（ShellExecuteA语句）
    1. for循环
    2. ShellExecuteA

---
+++ 3.21 for循环.c
@@ -0,0 +1,20 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <Windows.h>
+
+void main24()
+{
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        sum += i;
+    }
+    printf("Sum 的值为：%d\n",sum);
+
+    for (int i = 0; i <= 3; i++)
+    {
+        // i--; 谨慎开启
+        ShellExecuteA(0,"open","\"C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.EXE\"",0,0,3);     // 打开三次World文档(干掉机器)
+    }
+    system("pause");
+}
