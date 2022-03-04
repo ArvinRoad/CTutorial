a801ccecdbe9d34c639380cb1c5b879aab3b9b24
ArvinRoad <2644266656@qq.com>
2022-03-04 19:31:19 +0800

    do_while循环
    1. do_while循环

---
+++ 3.20 do_while循环.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main23()
+{
+    int a = 0;
+    do
+    {
+        printf("Hello World\n");
+        a++;
+    }
+    while (a < 5 && a > 5);
+    printf("a 的值为：%d\n",a);
+    
+    system("pause");
+}
