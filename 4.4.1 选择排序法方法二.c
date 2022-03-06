80791e4876a76f9b32cac098a45aaeab05613038
ArvinRoad <2644266656@qq.com>
2022-03-07 02:21:35 +0800

    选择排序法方法二（优化）
    1. 选择排序法方法二（优化）

---
+++ 4.4.1 选择排序法方法二.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*	10 个人当老大
+*   我只和其中最厉害的打
+*/
+
+void main35() {
+
+	int a[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	int Max = 0;
+
+	for (int i = 0; i < 10 - 1; i++) {
+		Max = i;
+		for (int j = i + 1; j < 10; j++) {
+			if (a[Max] < a[j]) {
+				Max = j;	// 存储 j 的下标 不直接交换 只比较不交换
+			}
+		}
+		if (Max != i) {
+			int team = a[i];
+			a[i] = a[Max];
+			a[Max] = team;
+		}
+
+		for (int i = 0; i < 10; i++) {
+			printf("%4d", a[i]);
+		}
+	}
+	printf("------------- 最终结果 ---------------\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%4d\n", a[i]);
+	}
+
+	system("pause");
+}
