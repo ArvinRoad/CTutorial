2162970152c1dd5830063bb79a500de958612268
ArvinRoad <2644266656@qq.com>
2022-03-06 14:13:44 +0800

    函数（跨文件函数）
    1. 函数源文件
    2. 函数头文件

---
+++ 4.0 函数.c
@@ -0,0 +1,22 @@
+﻿#include "4.0 函数.h"
+
+/* 函数的定义 */
+void add(int a,int b);
+
+int main30() {
+
+	int num1,num2;
+	printf("请输入数值a：");
+	scanf("%d", &num1);
+	printf("\n请输入数值b：");
+	scanf("%d", &num2);
+	add(num1,num2);
+	Helllo();
+	system("pause");
+	return 30;
+}
+
+void add(int a, int b) {
+	int c = a + b;
+	printf("a+b = %d\n", c);
+}
---
+++ 4.0 函数.h
@@ -0,0 +1,8 @@
+﻿#pragma once	// 防止头文件重复包含
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void Helllo() {
+	printf("Hello World\n");
+}
