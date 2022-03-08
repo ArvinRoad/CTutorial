97cc6d5ac1782c3b185aa1ee94164026e15938ee
ArvinRoad <2644266656@qq.com>
2022-03-08 21:51:27 +0800

    内存与变量
    1. 内存与变量（外挂原理介绍）

---
+++ 5.0 内存与变量.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+
+	// a其实的地址的别名
+	int a = 10;
+	printf("%p\n", &a);
+
+	// 这里断点，如何打开内存窗口编辑值再运行。实际上外挂就是通过这样来工作的
+
+	printf("%d\n", a);
+
+	system("pause");
+}
