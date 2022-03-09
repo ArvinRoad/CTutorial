1ce1a5420c5d68acf50eb52f1f8d9b6539d3de2b
ArvinRoad <2644266656@qq.com>
2022-03-09 16:48:39 +0800

    间接修改内存地址
    1. 间接修改内存地址

---
+++ 5.7 间接修改内存地址.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*	采用两种方式来修改变量：直接与间接(提供指针)
+*/
+
+void main46() {
+	int a = 10;	// 变量名可以看作内存地址的别名
+	a = 1;		// 直接修改
+	printf("a = %d\n", a);
+
+	int* p = &a;	// 变量地址相当于身份证号，拿取 a 的内存地址
+	printf("p = %p &a = %p\n", p, &a);
+
+	*p = 100;	// 间接 提供内存地址间接修改变量
+	printf("a = %d\n", a);
+
+	printf("\n");
+	system("pause");
+}
