626fb36c50b1ea18d06f97041c6c3d018839684a
ArvinRoad <2644266656@qq.com>
2022-03-10 20:55:31 +0800

    内存栈区疑问与调试
    1.  内存栈区疑问与调试

---
+++ 5.10 内存栈区疑问与调试.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*	两个函数声明同一个变量，这两个变量有关系吗？ ：没有
+*/
+
+char* p_str2();
+
+void main49(void) {
+
+	char* p = NULL;
+	p = p_str2();
+
+	printf("p = %s\n", p);
+	printf("p = %p\n", p);
+
+	// 在这里再打一个断点运行查看内存
+	printf("\n");
+	system("pause");
+}
+
+char* p_str2() {
+	char str[100] = "Hello";
+	printf("str = %s\n", str);
+	printf("str = %p\n", str);
+	// 查看栈销毁，在这里打一个断点
+	return str;
+}
