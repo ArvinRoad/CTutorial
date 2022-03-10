8133f457ba8ab6e68b2335f7570973368f426caa
ArvinRoad <2644266656@qq.com>
2022-03-11 04:19:16 +0800

    指针数组
    1. 指针数组
    备注：
    指针数组就是一个指针类型的数组

---
+++ 5.15 指针数组.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void main54(void) {
+	int a = 1;
+	int b = 2;
+	int c = 3;
+
+	int d[4] = { 1,2,3,4 };
+
+	for (int i = 0; i < 4; i++) {
+		printf("%d\n", d[i]);
+	}
+
+	char *pa[] = { "a","b","c" };	// 指针数组 "a"在指针常量区，"a"存储的实际上是地址
+	// 当然还有一种叫数组指针，实际上就是指向数组的指针
+	// 注意：指针数组是指针类型的数组，数组指针是指向数组的指针
+	// 断点
+	for (int i = 0; i < 3; i++) {	// 这里+1是加了一个地址的长度
+		printf("%s\n", pa[i]);	// 这里i也是一个地址的长度
+	}
+	printf("\n");
+	system("pause");
+}
