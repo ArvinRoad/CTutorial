e9f7f35fac39775ada4b4e128fd7d8eee3705169
ArvinRoad <2644266656@qq.com>
2022-03-13 15:57:27 +0800

    C语言文件基本操作
    1. C语言文件基本操作
    备注：
    fclose(fp)；锁门

---
+++ 6.0 C语言文件基本操作.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main64(void) {
+
+	FILE* fp = NULL;	// 可以将指针变量看成一个钥匙
+	fp = fopen("FILE\\Hello.txt", "w+");	// 第一个参数是"文件路径和文件名称" 第二个参数是模式 开门
+	if (fp == NULL) {
+		return -1;
+	}
+
+	fclose(fp);	// 关门
+	fp = NULL;
+
+	printf("\n");
+	system("pause");
+}
