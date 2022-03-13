02652abbdf2176e63881cb086c275e53029256ef
ArvinRoad <2644266656@qq.com>
2022-03-13 19:31:37 +0800

    文件重定向二
    1. 文件重定向二
    2. FILE 类型的底层实现
    备注：
    fgets 是gets的替换版，三个参数：内存地址、可存储最大字节数、文件流，当然还有一个fputs输出配合使用。

---
+++ 6.3 文件重定向二.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main67(void) {
+    char ch[10]={0};
+    
+    // gets(ch);
+    /* fgets 是gets的替换版，三个参数：内存地址、可存储最大字节数、文件流 */
+    fgets(ch, 10, stdin);
+
+    printf("%s\n",ch);
+
+    fputs(ch, stdout);
+    
+    printf("\n");
+    system("pause");
+}
---
+++ 6.3.1 FILE 类型的底层实现.c
@@ -0,0 +1,15 @@
+﻿/**
+*	C语言通过该结构体描述文件
+*/
+
+struct _iobuf {
+	char* _ptr;		// 当前缓冲区内容指针
+	int _cnt;		// 缓冲区还有多少字符
+	char* _base;	// 缓冲区的起始地址
+	int _flag;		// 文件流的状态，是否错误或结束
+	int _file;		// 文件描述符
+	int _charbuf;	// 双字节缓存，缓存两个字节
+	int _bufsiz;	// 缓冲区大小
+	char* _tmpfname;// 临时文件名
+};
+typedef struct _iobuf FILE;
