5d44deaed626b7f83f4fc8b6555490e027547af0
ArvinRoad <2644266656@qq.com>
2022-03-13 20:20:18 +0800

    C语言读取文件内容
    1. C语言读取文件内容

---
+++ 6.4 C语言读取文件内容.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main68(void) {
+	char ch = 0;
+	FILE* fp = fopen("FILE\\World.txt", "r");
+	if (fp == NULL) {
+		return -1;
+	}
+
+	while (!feof(fp)) {
+		ch = fgetc(fp);	// 获取，它一次只能读取一个字符
+		printf("%c", ch);
+	}
+	
+	fclose(fp);
+	fp = NULL;
+
+	printf("\n");
+	system("pause");
+}
