038e5f05752a28958353e46bc68ddc07ff67ff43
ArvinRoad <2644266656@qq.com>
2022-03-13 23:47:33 +0800

    C语言获取文件大小
    1.  C语言获取文件大小
    备注：
    fseek(pf, 0, SEEK_END);	// 文件指针移动到末尾
    int length = ftell(pf); // 获取当前文件指针距离开头的长度

---
+++ 6.5 C语言获取文件大小.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main69(void) {
+
+	FILE* pf = fopen("FILE\\World.txt", "rb");
+	
+	if (pf == NULL) {
+		return -1;
+	} else {
+		/* 0代表偏移量 */
+		fseek(pf, 0, SEEK_END);	// 文件指针移动到末尾
+		int length = ftell(pf); // 获取当前文件指针距离开头的长度
+		fclose(pf);
+		printf("%d\n", length);
+		pf = NULL;
+		return length;
+	}
+
+
+	printf("\n");
+	system("pause");
+} 
